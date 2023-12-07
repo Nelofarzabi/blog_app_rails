eb252e7a45feef1d89c05cb66c98d2f22538d7d5
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-12-07 21:02:02 +0430

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Define root path
+  root to: 'users#index'
+  
+  # MY CODE
+  # get '/users', to: "users#index"
+  # get '/users/:id', to: "users#show"
+  # get '/users/:user_id/posts', to: "posts#index"
+  # get '/users/:user_id/posts/:id', to: "posts#show"
+  
+  #CODE REVIEWER optional comment
+  # [OPTIONAL] While this is going to work fine, I will encourage you to take advantage of resources by having something like:
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
